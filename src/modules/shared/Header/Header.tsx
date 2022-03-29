fe8248b2dcfd67ab725026f7d95bc15ad597ae49
Dawid Tomczak <dawid.tomczak@allegro.pl>
2022-03-29 21:42:51 +0200

    i18n translations

--- src/modules/shared/Header/Header.tsx
+++ src/modules/shared/Header/Header.tsx
@@ -1,18 +1,32 @@
 import { Button, Typography } from '@mui/material';
 import styles from './Header.module.css';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const { t, i18n } = useTranslation();
+
+  const languages: string[] = ['pl', 'ua'];
+
   return (
     <header className={styles.header}>
-      <Typography className={styles.header__title}>Kartka z areny</Typography>
+      <Typography className={styles.header__title}>
+        {t('Kartka z areny')}
+      </Typography>
       <div className={styles.header__language}>
-        <Button variant='outlined' className={styles.language__button}>
-          PL
-        </Button>
-        <Button variant='outlined' className={styles.language__button}>
-          UA
-        </Button>
+        {languages.map((lang) => {
+          return (
+            <Button
+              key={lang}
+              variant='outlined'
+              className={styles.language__button}
+              onClick={() => i18n.changeLanguage(lang)}
+              disabled={i18n.resolvedLanguage === lang}
+            >
+              {lang.toUpperCase()}
+            </Button>
+          );
+        })}
       </div>
     </header>
   );
