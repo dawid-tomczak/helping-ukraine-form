56b145051ed81269519f47e2bdaa09079e6475cd
Dawid Tomczak <dawid.tomczak@allegro.pl>
2022-03-31 19:25:34 +0200

    translations in json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationPL from './locales/pl/translation.json';
+import translationUK from './locales/uk/translation.json';
 
 i18n
   // detect user language
@@ -12,36 +14,16 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: 'ua',
+    fallbackLng: 'PL',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
       PL: {
-        translation: {
-          'Kartka z areny': 'Kartka z areny',
-          'Czego brakuje?': 'Czego brakuje?',
-          Produkt: 'Produkt',
-          Chleb: 'Chleb',
-          Mleko: 'Mleko',
-          Płatki: 'Płatki',
-          Bułki: 'Bułki',
-          Ilość: 'Ilość',
-          Prześlij: 'Prześlij',
-        },
+        translation: translationPL,
       },
       UK: {
-        translation: {
-          'Kartka z areny': 'Листівка з арени',
-          'Czego brakuje?': 'Чого не вистачає?',
-          Produkt: 'Продукт',
-          Chleb: 'Хліб',
-          Mleko: 'Молоко',
-          Płatki: 'Зернові',
-          Bułki: 'Булочки',
-          Ilość: 'Кількість',
-          Prześlij: 'Подати',
-        },
+        translation: translationUK,
       },
     },
   });
