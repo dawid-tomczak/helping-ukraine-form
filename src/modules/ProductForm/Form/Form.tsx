50ec215a57cfb236c118b4fb2c7be74fecc27e8a
Dawid Tomczak <dawid.tomczak@allegro.pl>
2022-04-04 13:32:47 +0200

    progress on forms

--- src/modules/ProductForm/Form/Form.tsx
+++ src/modules/ProductForm/Form/Form.tsx
@@ -1,106 +1,157 @@
-import React, { ReactElement, useState } from 'react';
-import styles from './Form.module.css';
+import React, { ReactElement, useEffect, useState } from 'react';
 import {
-  Autocomplete,
   Button,
   FormControl,
   FormControlLabel,
+  FormGroup,
   FormLabel,
+  Grid,
+  Input,
+  InputLabel,
+  MenuItem,
   Radio,
   RadioGroup,
+  Select,
   Stack,
-  TextField,
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { Category } from '../../../@types/helpers/Category';
 import { Product } from '../../../@types/helpers/Product';
-
 interface Props {
   categories: Category[];
   products: Product[];
 }
 
+const formVariant = 'standard';
+
 const Form = ({ categories, products }: Props): ReactElement => {
   const { t, i18n } = useTranslation();
 
-  const [nameInput, setNameInput] = useState<string | null>(null);
-  const [categoryInput, setCategoryInput] = useState<string | null>(null);
-  const [productInput, setProductInput] = useState<string | null>(null);
-  const [amountInput, setAmountInput] = useState<string | null>(null);
+  const [nameInput, setNameInput] = useState<string>('');
+  const [categoryInput, setCategoryInput] = useState<string>('');
+  const [productInput, setProductInput] = useState<string>('');
+  const [amountInput, setAmountInput] = useState<string>('');
+
+  const availableProducts: Product[] =
+    categoryInput !== ''
+      ? products.filter((product) => product.categoryId === categoryInput)
+      : [];
 
   const resetForm = (fullReset = false) => {
-    setCategoryInput(null);
-    setProductInput(null);
-    setAmountInput(null);
+    setCategoryInput('');
+    setProductInput('');
+    setAmountInput('');
 
     if (fullReset) {
-      setNameInput(null);
+      setNameInput('');
     }
   };
 
+  const handleSubmit = (): void => {
+    const elementToAdd = {
+      name: nameInput,
+      category: categoryInput,
+      product: productInput,
+      amount: amountInput,
+      inputLang: i18n.language,
+    };
+
+    console.log('elementToAdd :>> ', elementToAdd);
+
+    resetForm();
+  };
+
   return (
-    <>
-      <form className={styles.form}>
-        <Stack spacing={3} alignItems={'center'}>
-          <TextField
-            className={styles.form__input}
-            label={t('Imię')}
-            variant='outlined'
+    <FormGroup>
+      <Stack spacing={3}>
+        <FormControl variant={formVariant} required>
+          <InputLabel htmlFor='name-input'>{t('Imię')}</InputLabel>
+          <Input
             value={nameInput}
             onChange={(event) => setNameInput(event.target.value)}
+            id='name-input'
+            aria-describedby='name-input'
+            required
           />
+        </FormControl>
 
-          <FormControl>
-            <FormLabel>{t('Kategoria')}</FormLabel>
-            <RadioGroup
-              value={categoryInput}
-              onChange={(event) => setCategoryInput(event.target.value)}
-            >
-              {categories.map((category) => {
-                return (
-                  <FormControlLabel
-                    key={category.id}
-                    value={category.id}
-                    control={<Radio />}
-                    label={category.name[i18n.language as 'PL' | 'UK']}
-                  />
-                );
-              })}
-            </RadioGroup>
-          </FormControl>
-
-          <Autocomplete
-            className={styles.form__input}
-            options={products}
-            getOptionLabel={(option) =>
-              option.name[i18n.language as 'PL' | 'UK']
-            }
-            renderInput={(params) => (
-              <TextField {...params} label={t('Produkt')} />
-            )}
-            value={products.find((e) => e.id === productInput)}
-            onChange={(event, input) => setProductInput((input as Product).id)}
-          />
-
-          <TextField
-            className={styles.form__input}
-            label={t('Ilość')}
-            variant='outlined'
-            type='number'
-            value={amountInput}
-            onChange={(event) => setAmountInput(event.target.value)}
-          />
-
-          <Button
-            className={styles.form__button}
-            variant='contained'
-            onClick={() => resetForm()}
+        <FormControl variant={formVariant} required>
+          <FormLabel>{t('Kategoria')}</FormLabel>
+          <RadioGroup
+            value={categoryInput}
+            onChange={(event) => setCategoryInput(event.target.value)}
           >
-            {t('Prześlij')}
-          </Button>
-        </Stack>
-      </form>
-    </>
+            {categories.map((category) => {
+              return (
+                <FormControlLabel
+                  key={category.id}
+                  value={category.id}
+                  control={<Radio />}
+                  label={category.name[i18n.language as 'PL' | 'UK']}
+                />
+              );
+            })}
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl variant={formVariant} required>
+          <InputLabel htmlFor='product-input-label'>{t('Produkt')}</InputLabel>
+          <Select
+            labelId='product-input-label'
+            id='product-input'
+            value={productInput}
+            label={t('Produkt')}
+            onChange={(event) => setProductInput(event.target.value)}
+          >
+            {availableProducts.map((category) => {
+              return (
+                <MenuItem key={category.id} value={category.id}>
+                  {category.name[i18n.language as 'PL' | 'UK']}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+
+        <FormControl variant={formVariant} required>
+          <InputLabel htmlFor='amount-input'>{t('Ilość')}</InputLabel>
+          <Input
+            value={amountInput}
+            type='number'
+            onChange={(event) => setAmountInput(event.target.value)}
+            id='amount-input'
+            aria-describedby='amount-input'
+          />
+        </FormControl>
+        <Grid container justifyContent='flex-end' columns={10}>
+          <Grid
+            item
+            xs={5}
+            md={1}
+            sx={{ display: 'flex', justifyContent: 'center' }}
+          >
+            <Button
+              variant='text'
+              color='error'
+              onClick={() => resetForm(true)}
+            >
+              {t('Wyczyść')}
+            </Button>
+          </Grid>
+
+          <Grid
+            item
+            xs={5}
+            md={1}
+            sx={{ display: 'flex', justifyContent: 'center' }}
+          >
+            <Button variant='contained' onClick={handleSubmit} type='submit'>
+              {t('Prześlij')}
+            </Button>
+          </Grid>
+        </Grid>
+      </Stack>
+    </FormGroup>
   );
 };
 
